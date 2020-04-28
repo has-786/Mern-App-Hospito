c76aaa9a6e5e81d5a8411dd586c5e690bc37f7f6
syed hasnain <syedhasnain9163@gmail.com>
2020-04-28 10:27:25 +0530

    Final Commit 8

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 var mongoose=require('mongoose');
 var express=require('express');
-var bcrypt=require('bcrypt');
-var nodemailer=require('nodemailer');
-randomstring=require('randomstring');
+//var bcrypt=require('bcrypt');
+//var nodemailer=require('nodemailer');
+//var randomstring=require('randomstring');
 path=require('path');
-
+/*
+"google-map-react": "^1.1.5",
+    "mongoose": "^5.9.10",
+    "react": "^16.12.0",
+    "react-dom": "^16.12.0",
+    "react-geolocated": "^3.0.1",
+    "react-google-maps": "^9.4.5",
+    "react-router-dom": "^5.1.2",
+    "react-scripts": "3.2.0",
+    "typescript": "^3.8.3"  
+*/
 
 app=express();
 
@@ -16,12 +26,12 @@ app.get('*',(req,res)=>{
 });
 
 
-passport=require('passport');
-var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false }));   app.use(bodyParser.json());
+//passport=require('passport');
+//var bodyParser = require('body-parser');
+//app.use(bodyParser.urlencoded({ extended: false }));   app.use(bodyParser.json());
 
-require('./server/security/passport.js')(passport);   
-require('./server/routes/mainRoutes.js')(app,passport,bcrypt,nodemailer);
+//require('./server/security/passport.js')(passport);   
+//require('./server/routes/mainRoutes.js')(app,passport,bcrypt,nodemailer);
 
 var port = 8080;
 
